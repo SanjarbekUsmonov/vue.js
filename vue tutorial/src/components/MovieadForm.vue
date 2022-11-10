7c1ead368798ffd19146722fc3ba306565e2063e
SanjarbekUsmonov <noobpusk@gmail.com>
2022-11-10 16:24:30 +0500

    form

--- vue tutorial/src/components/MovieadForm.vue
+++ vue tutorial/src/components/MovieadForm.vue
@@ -1,16 +1,40 @@
 <template>
     <div class="movie-add-form">
        <h3>Yangi kino qo'shish</h3>
-       <form>
-            <input type="text" class="form-control new-movie-label" placeholder="Qanday kino">
-            <input type="text" class="form-control new-movie-label" placeholder="Necha marotaba ko'rilgan">
-            <button class="btn btn-outline-dark" type="submit">Qo'shish</button>
+       <form class="d-flex" @submit.prevent>
+            <input 
+            type="text" 
+            class="form-control new-movie-label" 
+            placeholder="Qanday kino" 
+            v-model="name">
+
+            <input 
+            type="text" 
+            class="form-control new-movie-label" 
+            placeholder="Necha marotaba ko'rilgan" 
+            v-model="viewers">
+            <button class="btn btn-outline-dark" type="submit" @click="addMovie">Qo'shish</button>
        </form>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+      return {
+        name: "",
+        viewers: ""
+      }
+    },
+    methods: {
+      addMovie(){
+        const newMovie = {
+          name: this.name,
+          viewers:this.viewers,
+          favorite: false
+        }
+        console.log(newMovie);
+      }
+    },
 }
 </script>
 <style scoped>
